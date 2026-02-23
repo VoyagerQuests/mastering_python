22952758a011bdf9e8db0b8b4a7ab7461770511d
Hanz <hanz@voyagernetz.com>
2026-02-23 12:14:53 -0600

    getting started

--- print_environment.py
+++ print_environment.py
@@ -14,14 +14,25 @@ print(f'\n{"site-packages:":<{LABEL_WIDTH}}')
 for p in getattr(site, "getsitepackages", lambda: [])():
     print(f'{"":<{LABEL_WIDTH}}\t{p}')
 
-print(f'\n{"user site-packages:":<{LABEL_WIDTH}}\t{site.getusersitepackages()}')
 
 print(f'\n{"full sys.path:":<{LABEL_WIDTH}}')
 for p in sys.path:
     print(f'{"":<{LABEL_WIDTH}}\t{p}')
 
+print("\n")
 print("*** Installed Python Packages ***\n")
-for dist in sorted(distributions(), key=lambda d: d.metadata["Name"].lower()):
-    name = dist.metadata["Name"]
-    version = dist.version
-    print(f"{name}=={version}")
+print(f'Found {len(list(distributions()))} installed packages:\n')
+# for dist in sorted(distributions(), key=lambda d: d.metadata["Name"].lower()):
+#     name = dist.metadata["Name"]
+#     version = dist.version
+#     print(f"{name}=={version}")
+
+# Terminal commands
+#
+# python -c "import sys; print(sys.executable)"  # prints the path to the Python executable
+# python -c "import sys; print(sys.prefix)"  # prints the prefix of the Python installation
+# python -c "import sys; print(sys.base_prefix)"  # prints the base prefix of the Python installation
+# python -c "import sys; print(sys.prefix != sys.base_prefix)"  # prints True if in a virtual environment, False otherwise
+# python -c "import site; print(site.getsitepackages())"  # prints the list of site-packages directories
+# python -c "import sys; print(sys.path)"  # prints the full sys.path
+# python -m pip list  # lists all installed Python packages with their versions
